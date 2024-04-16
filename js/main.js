0c196dd46a44bdfabc107b3f9854aa7fbcb7b901
Bigas Garcia <bigasdav@amazon.es>
2024-04-16 15:46:58 +0200

    third update

--- js/main.js
+++ js/main.js
@@ -24,10 +24,28 @@ if (hacker1.length > hacker2.length) {
 }
 
 const Name = "John";
-for (const value of Name){
-    console.log(value.toUpperCase());
+for (const value of Name) {
+  console.log(value.toUpperCase());
 }
 
-for (let i = 0; i < 5; i++){
-console.log(Name[i])
+for (let i = Name.length - 1; i >= 0; i--) {
+  console.log(Name[i]);
 }
+
+// Preguntar acerca del punto 3.3 - Problemas con el orden lexicográfico
+const sentences = [
+  "El nombre del conductor va primero",
+  "¿Qué? ¿Los dos tienen el mismo nombre?",
+  "Yo, el navegador va primero definitivamente",
+];
+
+sentences.sort();
+
+console.log(sentences);
+// Preguntar acerca del punto 3.3 - Problemas con el orden lexicográfico
+
+const Text = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse congue, diam quis bibendum tempus, ex justo gravida neque, sed venenatis lectus purus sed tortor. Proin dapibus consectetur risus a dapibus. Etiam massa metus, vestibulum a consequat laoreet, maximus nec lorem. Suspendisse molestie, odio eget semper finibus, orci nisl mattis erat, id suscipit neque quam ac ligula. Donec nisl odio, placerat eu venenatis eu, dictum quis ex. Mauris nunc ex, faucibus eget est ac, mollis eleifend eros. Morbi varius ultricies lacus. Vivamus maximus dui in mauris egestas, quis euismod ipsum egestas. Aenean iaculis, dolor eget placerat tincidunt, ligula erat congue ex, sed lacinia arcu nisi semper nibh. Integer facilisis elit id urna egestas vulputate. Quisque pulvinar lacus at scelerisque scelerisque. Suspendisse euismod consequat mauris. Ut sit amet dui iaculis, tincidunt purus at, maximus leo. Fusce placerat, ipsum non consequat molestie, ligula tellus iaculis enim, nec accumsan eros felis nec quam. Proin viverra sapien non sagittis vestibulum. Nunc lectus mi, dictum eget sagittis id, sagittis at justo. In elementum at lacus ut faucibus. Nulla lacinia tempus neque, ut mattis mi tristique ut. Nam vel massa at purus euismod elementum in sed purus. Nam convallis vitae quam et tempor. Nunc commodo dictum nisl eu ornare. Donec feugiat ipsum gravida purus luctus placerat. Morbi consequat mi augue, et fringilla dolor porttitor a. Cras blandit, eros ut rhoncus venenatis, orci massa posuere libero, id lacinia leo eros id elit. In iaculis augue non porta tempus. In rutrum nisl felis, consequat bibendum erat laoreet non. Nam posuere augue at ex imperdiet condimentum. Aliquam vestibulum elit tortor, venenatis ultricies felis ullamcorper fermentum. Maecenas risus mauris, pharetra at tristique vitae, pulvinar non leo. Vivamus dignissim mi non ante tempor posuere.",
+];
+
+Text.split(Text.length)
